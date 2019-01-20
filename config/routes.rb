1d57d3b8e9591c4c274ba9bceb35cba557b43d47
ihara moeka <ihara.moeka@gmail.com>
2019-01-21 05:52:47 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users
+  root 'murmurs#index'
   get '/tops', to: 'tops#index'
   resources :murmurs do
     collection do
       post :confirm
     end
+  end
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 end
